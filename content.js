a2979ab84d6722bd3c645d6d1f4067ccadfe434d
gracefulPotato <grace.ohair.sherman@gmail.com>
2017-01-19 09:02:07 -0800

    Alert pops up on every page and all p elements turn red

---
+++ content.js
@@ -0,0 +1,6 @@
+console.log("hello");
+alert("Hello from your Chrome extension!");
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"
+$("p").css('color','red');
+//tooltip({
+//content:"Contnet!"});
